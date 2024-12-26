452c707d5d4eaf119f25c9533d32e1f7f4fc1468
Saayemm <sayemdcc@gmail.com>
2024-12-26 23:39:16 +0600

    default

--- validation/validation.js
+++ validation/validation.js
@@ -1,6 +1,21 @@
-const number = 23;
+const number = "23";
 if(typeof number === "number"){
-    console.log("is a number");
-    
+    // console.log("is a number");
+}else{
+    // console.log("is not a number");
 }
 
+// ES6 => var, let, const
+
+// default.js
+
+function add(num1=4, num2=6){
+    const result = num1+num2
+    console.log(num1, num2, result);
+    
+    return result;
+}
+
+// add(4,5)
+add(4)
+add()
