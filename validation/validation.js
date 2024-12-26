b68e0167868db67dd0d79a3e8f6406c2304414bb
Saayemm <sayemdcc@gmail.com>
2024-12-27 01:05:37 +0600

    spread

--- validation/validation.js
+++ validation/validation.js
@@ -11,7 +11,7 @@ if(typeof number === "number"){
 
 function add(num1=4, num2=6){
     const result = num1+num2
-    console.log(num1, num2, result);
+    // console.log(num1, num2, result);
     
     return result;
 }
@@ -19,3 +19,18 @@ function add(num1=4, num2=6){
 // add(4,5)
 add(4)
 add()
+
+// Template
+
+// spread
+const number5 = [5, 6, 6, 7, 8, 9, 32]
+const max = Math.max(...number5)
+// console.log(...number5);
+// console.log(max);
+
+const num2 = [1,3,4,5,6,7]
+const num3  = num2;
+num3.push(90)
+console.log(num2);
+console.log(num3);
+
