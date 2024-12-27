402b645cb5cebc63aac928d107416383b6d45a7f
Saayemm <sayemdcc@gmail.com>
2024-12-27 13:57:38 +0600

    chain

---
+++ ES6/es6.js
@@ -0,0 +1,12 @@
+// Chain
+const data = [{name: "lok", id:123, adress: "Demra"}]
+// console.log(data[0].adress);
+
+const product = {
+    count: 600,
+    data: [
+        {name: "lenovo", price: 6000, model: "99jh"},
+        {name: "apple", price: 66000, model: "uoi9jh"},
+    ]
+}
+console.log(product.data[1].price);
