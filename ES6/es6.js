2f2bf01329e74ff0aa62689cc1e96270adfe1706
Saayemm <sayemdcc@gmail.com>
2024-12-27 15:42:58 +0600

    map

--- ES6/es6.js
+++ ES6/es6.js
@@ -9,4 +9,22 @@ const product = {
         {name: "apple", price: 66000, model: "uoi9jh"},
     ]
 }
-console.log(product.data[1].price);
+// console.log(product.data[1].price);
+
+// map
+const number = [3,4,5,6,7,8,9,1]
+// const doubleIt = num => num*2
+// const result = number.map(doubleIt)
+// console.log(result);
+const result = number.map(num=>num*2)
+// console.log(result);
+
+const friends = ["rony",  "prithy", "nazim", "sakib"]
+const length = friends.map(friend=>friend.length)
+// console.log(length);
+const firstLetter = friends.map(friend=>friend[0])
+// console.log(firstLetter);
+
+
+
+
