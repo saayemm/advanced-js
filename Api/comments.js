ff4e2b0c069e6e87c86dc227138e27fe3566c330
Saayemm <sayemdcc@gmail.com>
2024-12-29 00:00:19 +0600

    json

---
+++ Api/comments.js
@@ -0,0 +1,13 @@
+const loadComments = () => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+        .then(res=> res.json())
+        .then(data=> console.log(data))
+        .catch(error=> console.error("error found boss", error))
+}
+
+const loadComments2 = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/albums")
+    const data = await res.json()
+    console.log(data);
+    
+}
