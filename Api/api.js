d61b1a74a880ccef07e2518513a35c6b31d98dd2
Saayemm <sayemdcc@gmail.com>
2024-12-28 11:55:29 +0600

    json data

--- Api/api.js
+++ Api/api.js
@@ -1,7 +1,23 @@
 // Json
-const user = { id: 3, name:"loki", adress: "Fomh"}
-const stringified = JSON.stringify(user)
+// const user = { id: 3, name:"loki", adress: "Fomh"}
+// const stringified = JSON.stringify(user)
 
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
 
+// String to object => Json.parse
+// Object to string => Json.Stringfy
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+
+function loadData(){
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+}
