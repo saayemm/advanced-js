0c6ff8cb08ca2eca764430e51f222ffc8d0c390e
Saayemm <sayemdcc@gmail.com>
2024-12-28 22:31:29 +0600

    api call

--- Api/app.js
+++ Api/app.js
@@ -11,7 +11,39 @@ function loadUsers(){
     )
 }
 
+const ul = document.querySelector(".container")
+
 function displayUser(data){
-    console.log(data);
+   
+   for(const user of data){
+    const li = document.createElement("li")
+    li.innerText = user.email
+    ul.appendChild(li)
+   }
+   }
     
+
+
+function displayPost(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res=>res.json())
+    .then(data=> displayData(data))
 }
+const postElem = document.getElementById("post-container")
+
+function displayData(posts){
+    
+    for(const post of posts){
+        console.log(post);
+        
+        const div = document.createElement("div")
+        div.classList.add("post")
+        div.innerHTML = `
+        <h3>User: ${post.id} </h3>
+        <h4>Title: ${post.title}</h4>
+        <p>Drscription: ${post.body}<p/>
+        `
+        postElem.appendChild(div)
+    }
+}
+displayPost()
