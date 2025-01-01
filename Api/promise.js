e4a3d4839f3863a269904b12e614ebd06f8b33c3
Saayemm <sayemdcc@gmail.com>
2025-01-01 16:37:08 +0600

    local

--- Api/promise.js
+++ Api/promise.js
@@ -1,3 +1,11 @@
 const myLoader = () => {
-    return new Promise((resolve, reject))
+    return new Promise((resolve, reject) => {
+        const success = true;
+        if(success){
+            resolve("Data resolved")
+        }else{
+            reject("Your data is rejected")
+        }
+    })
 }
+myLoader()
