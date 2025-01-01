43f84dfb9b5282d70839f576daf6a0192181c1cc
Saayemm <sayemdcc@gmail.com>
2025-01-01 17:18:24 +0600

    local store

--- cart/cart.js
+++ cart/cart.js
@@ -9,5 +9,44 @@ const addProduct = () => {
     productQuantity.value = "";
 
     console.log(product, quantity);
+
+    displayProduct(product, quantity)
+
+    saveProductLocalStroage(product, quantity)
     
 }
+
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById("product-container")
+    const li = document.createElement("li")
+    li.innerHTML = `${product}: ${quantity}`
+
+    ul.appendChild(li)
+}
+
+const getItemShoppingCart = () => {
+  
+    const storeCart = localStorage.getItem("cart")
+    let cart = {}
+    if(storeCart){
+        cart = JSON.parse(storeCart)
+    }
+    return cart;
+}
+
+const saveProductLocalStroage = (product, quantity) => {
+    const cart = getItemShoppingCart()
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem("cart", cartStringified)
+}
+
+const displayProductFromLocalStroage = () => {
+    const savedCart = getItemShoppingCart()
+    for(const product in savedCart){
+        const quantity = savedCart[product]
+        console.log(product, quantity)
+        displayProduct(product, quantity)
+    }
+}
+displayProductFromLocalStroage()
